758aeaa57796b7ef6c35e89d00a255a1fbc1634f
Lily Gasztowtt <lilyjoannag@gmail.com>
2018-11-19 15:25:40 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'questions/ask', to: 'questions#ask'
+  get '/answer', to: 'questions#answer'
+  # post 'answer', to: 'answers#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
